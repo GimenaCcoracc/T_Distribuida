e1e57e8082d11f0ca2f5aa536e9be373dd474f62
GimenaCcoracc <gimena.ccoracc@gmail.com>
2025-05-01 16:29:55 +0000

    avance v7

--- src/main/java/pe/edu/vallegrande/workshop/config/SecurityConfig.java
+++ src/main/java/pe/edu/vallegrande/workshop/config/SecurityConfig.java
@@ -17,28 +17,29 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(auth -> auth
-                        .pathMatchers("/swagger-ui.html").permitAll()
-                        .anyExchange().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
-                )
-                .cors(cors -> cors
-                        .configurationSource(exchange -> {
-                            var config = new org.springframework.web.cors.CorsConfiguration();
-                            config.setAllowCredentials(true);
-                            // Especifica el origen permitido
-                            config.addAllowedOrigin("http://localhost:4200");
-                            config.addAllowedHeader("*");
-                            config.addAllowedMethod("*");
-                            return config;
-                        })
-                )
-                .build();
-    }
+        @Bean
+        public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+            return http
+                    .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                    .authorizeExchange(auth -> auth
+                            .pathMatchers("/swagger-ui.html", "/api/workshops/active").permitAll()
+                            .anyExchange().authenticated()
+                    )
+                    .oauth2ResourceServer(oauth2 -> oauth2
+                            .jwt(Customizer.withDefaults())
+                    )
+                    .cors(cors -> cors
+                            .configurationSource(exchange -> {
+                                var config = new org.springframework.web.cors.CorsConfiguration();
+                                config.setAllowCredentials(true);
+                                // Especifica el origen permitido
+                                config.addAllowedOrigin("http://localhost:4200"); // Cambia esto si tu frontend está en otro origen
+                                config.addAllowedHeader("*");
+                                config.addAllowedMethod("*");
+                                return config;
+                            })
+                    )
+                    .build();
+        }
+    
 }
